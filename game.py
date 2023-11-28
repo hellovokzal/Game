fb7d57fdadde9354680864a23a81df7a20651483
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-11-28 21:13:59 +0500

    Update game.py

--- game.py
+++ game.py
@@ -7,21 +7,39 @@ app = Flask(__name__)
 def echo1():
     if request.method == 'POST':
         data = request.get_json()
-        site = post("https://acb.basketballrandom.net/demo", json=data)
-        return site.json()
+        site = post("{message}", data=data, timeout=15)
+        return site.text()
     else:
-        site = get("https://acb.basketballrandom.net/demo")
-        return site.text
+        site = get("{message}", timeout=15)
+        return site.text()
 
 @app.route("/<path:message>", methods=['GET', 'POST'])
 def echo(message):
     if request.method == 'POST':
         data = request.get_json()
-        site = post(f"https://acb.basketballrandom.net/{message}", json=data)
-        return site.json()
+        site = post(f"{message}", data=data, timeout=15)
+        return site.text()
     else:
-        site = get(f"https://acb.basketballrandom.net/{message}")
-        return site.text
+        site = get(f"{message}", timeout=15)
+        return site.text()
+
+@app.route("/favicon.ico")
+
+@app.errorhandler(500)
+
+def error(e):
+	return """
+ <!DOCTYPE html>
+<html>
+<head>
+  <title>500 Internal Server Error</title>
+</head>
+<body>
+  <h1>500 Internal Server Error</h1>
+  <p>Не удалось загрузить сайт!</p>
+</body>
+</html>
+"""
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
